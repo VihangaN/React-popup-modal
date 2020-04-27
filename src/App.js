6f550d0a8229de25799b87f96afddc2d97ed9808
vihanga nivarthana <vihanganivarthana@vihangas-MacBook-Air.local>
2020-04-27 21:51:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React ,{ Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+
+  state ={
+    active:false
+  }
+
+  
+
+  popUp =() =>{
+    this.setState({
+      active:!this.state.active
+      
+    })
+    
+    
+  }
+
+ 
+ 
+
+render(){
+  const isActive = this.state.active ? 'active' : '';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button className="trigger" onClick={this.popUp}>Trigger</button>
+      <div className={`modal ${isActive}`}>
+      <span className="confirm">Are you sure ?</span>
+      <div className="btnContainer">
+      <button>Delete</button>
+      <button>Cancel</button>
+      </div>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
